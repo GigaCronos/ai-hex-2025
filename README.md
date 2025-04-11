79a9b5e68b8c44659346c11d36c39bd105abc594
GigaCronos <jorgepichardocronos@gmail.com>
2025-04-11 06:55:43 -0400

    Not Empty

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ai-hex-2025
+
+## Estrategia: Minimax con heuristicas
+
+### EV(B,X)
+
+Definimos la evaluacion de un posible tablero B bajo la perspectiva de un jugador X -> Ev(B, X) como el resultado del siguiente algoritmo:
+
+1 - Tomar 1 camino ganador de tamaño minimo(donde las casillas ya marcadas por ese jugador aportan 0 a la "longitud" de dicho camino).
+
+2 - suponer que el contrario juega aleatoriamente y tiene una probabilidad de interrumpir ese camino, bajo esta supocion bloquear una casilla random del camino actual.
+
+3 - Repetir pasos 1 y 2 hasta tener 4 caminos distintos y sus longitudes.
+
+4 - Decimos que la probabilidad de que el contrario que juega aleatorio no interrumpa un camino es 
+$(total\_casillas - tamaño\_camino)/total\_casillas $ y la probabilidad de que si lo haga $(tamaño\_camino)/total\_casillas$. 
+
+5 - Siendo L la lista de las longitudes de los 4 caminos seleccionados y $tot$ el total de casillas la formula de Ev(B,X) seria $P[0]$ donde:
+    $$ P[i]=(tot-L[i])/tot + (L[i]/tot)*P[i+1]$$
+    $$ P[4]=0 $$
+
+
+### Score de una jugada
+Definimos el score de una jugada del jugador X, como $max(Ev(B,X), 1-Ev(B,Y))$ donde B es el estado del tablero luego de que X hace la jugada(Y es el otro jugador). Aqui suponemos que una jugada muy buena para el jugador Y es mala para el jugador X y viceversa, por eso nos quedamos con el maximo entre lo buena que sea esta jugada para X y lo mala que sea para Y.
+
+### Encontrando las casillas que vamos a explorar
+Exploraremos solo un conjunto reducido de jugadas en el Minimax, empezamos con las 8 que tengan mejor score y vamos reduciendo el ancho de exploracion de 2 en 2 a medida que descendemos hasta llegar a 0, donde no se explora más simplemente se devuelve la jugada de mayor score como resultado. El jugador para el cual queremos hallar la mejor jugada busca maximizar este score, mientras el otro busca minimizarlo, al tener altura impar(5) el ärbol de Minimax se garantiza que en el último nivel juegue el mismo jugador para el cual queremos hallar la mejor jugada.
---
+++ player.py
@@ -0,0 +1,164 @@
+import random
+import collections as stl
+
+class HexBoard:
+    def __init__(self, size: int):
+        self.size = size  # Tamaño N del tablero (NxN)
+        self.board = [[0 for _ in range(size)] for _ in range(size)]  # Matriz NxN (0=vacío, 1=Jugador1, 2=Jugador2)
+    
+    def clone(self) -> 'HexBoard':
+        pass
+
+    def place_piece(self, row: int, col: int, player_id: int) -> bool:
+        pass
+
+    def get_possible_moves(self) -> list:
+        pass
+    
+    def check_connection(self, player_id: int) -> bool:
+        pass
+
+
+class Player:
+    def __init__(self, player_id: int):
+        self.player_id = player_id  # Tu identificador (1 o 2)
+
+    def play(self, board: HexBoard) -> tuple:
+        raise NotImplementedError("¡Implementa este método!")
+    
+
+class myPlayer(Player):
+    def __init__(self, player_id: int):
+        self.player_id = player_id 
+
+    def play(self, board: HexBoard) -> tuple:
+        nboard=board.clone()
+        return self.get_best_move(nboard,self.player_id,4)[1]
+    
+    def get_best_move(self,board: HexBoard, player_id : int, depth: int ):
+        bests=[]
+        for i in range(0,board.size):
+            for j in range(0,board.size):
+                if board.board[i][j] == 0:
+                    bests.append((max(self.evaluate(board,player_id,i,j),1-self.evaluate(board,3-player_id,i,j)),(i,j)))
+        bests=sorted(bests,key=lambda tupla: tupla[0], reverse=True)
+        if(depth==0):
+            return bests[0]
+       
+       
+        if bests[0][0]>0.99999:
+            if player_id == self.player_id:
+                return bests[0]
+            else:
+                return (1-bests[0][0],bests[0][1])
+       
+        branch=2*depth
+        if len(bests)>branch:
+            bests=bests[:branch]
+        ans=(-1,(0,0))
+        if player_id!=self.player_id:
+            ans=(2,(0,0))
+        
+        for tup in bests:
+            board.board[tup[1][0]][tup[1][1]]=player_id
+            ntup=self.get_best_move(board,3-player_id,depth-1)
+            board.board[tup[1][0]][tup[1][1]]=0
+            if player_id == self.player_id:
+                if ans[0]<ntup[0]:
+                    ans=(ntup[0],tup[1])
+            else:
+                if ans[0]>ntup[0]:
+                    ans=(ntup[0],tup[1])
+                
+        return ans
+
+    def evaluate(self, board: HexBoard, player_id : int, row :int, col :int):
+        path_sizes=[]
+        originals=[]
+        board.board[row][col]=player_id
+        originals.append((row,col))
+        tot=board.size*board.size
+        for _ in range(0,4):
+            lis=self.find_path(board,player_id,row,col)
+            if lis is None:
+                path_sizes.append(tot)
+                break
+            
+            path_sizes.append(len(lis))
+            if len(lis)==0:
+                break
+            tup=random.choice(lis)
+            originals.append(tup)
+            board.board[tup[0]][tup[1]]=3-player_id
+        
+        for tup in originals:
+            board.board[tup[0]][tup[1]]=0
+
+        ans=0
+        mul=1
+        
+        for i in range(len(path_sizes)):
+            ans=ans+mul*(tot-path_sizes[i])/tot
+            mul=mul*path_sizes[i]/tot
+
+        return ans
+
+    def find_path(self, board: HexBoard,player_id: int, row : int, col: int):
+        Dir = [
+            (0, -1),   # Izquierda
+            (0, 1),    # Derecha
+            (-1, 0),   # Arriba
+            (1, 0),    # Abajo
+            (-1, 1),   # Arriba derecha
+            (1, -1)    # Abajo izquierda
+        ]
+
+        D=[[-1 for _ in range(board.size)] for _ in range(board.size)]
+        Q=stl.deque()
+        D[row][col]=-2
+        Q.append((row,col))
+        b1=-1
+        b2=-1
+        while Q.count()!=0:
+            u=Q[0]
+            Q.popleft()
+            for dir in Dir:
+                ndir=(u[0]+dir[0],u[1]+dir[1])
+                if ndir[0]<0 or ndir[0]>=board.size or ndir[1]<0 or ndir[1]>=board.size:
+                    continue
+                if board.board[ndir[0]][ndir[1]]==3-player_id:
+                    continue
+                if board.board[ndir[0]][ndir[1]]==0 and D[ndir[0]][ndir[1]]==-1:
+                    Q.append(ndir)
+                    D[ndir[0]][ndir[1]]=u[0]*board.size+u[1]
+                if board.board[ndir[0]][ndir[1]]==player_id and D[ndir[0]][ndir[1]]==-1:
+                    Q.appendleft(ndir)
+                    D[ndir[0]][ndir[1]]=u[0]*board.size+u[1]
+            
+            if u[0]==0 and player_id==2 and b1==-1:
+                b1=u[0]*board.size+u[1]
+            if u[0]==board.size-1 and player_id==2 and b2==-1:
+                b2=u[0]*board.size+u[1]
+            if u[1]==0 and player_id==1 and b1==-1:
+                b1=u[0]*board.size+u[1]
+            if u[1]==board.size-1 and player_id==1 and b2==-1:
+                b2=u[0]*board.size+u[1]  
+
+        if b1==-1 or b2==-1:
+            return None
+        
+        ans=[]
+        while b1!=-2:
+            r=b1//board.size
+            c=b1%board.size
+            if(board.board[r][c]==0):
+                ans.append((r,c))
+            b1=D[r][c]
+        while b2!=-2:
+            r=b2//board.size
+            c=b2%board.size
+            if(board.board[r][c]==0):
+                ans.append((r,c))
+            b2=D[r][c]
+
+        return ans
